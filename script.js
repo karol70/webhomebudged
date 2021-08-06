42d6c26f9ca714cf9d128fff43b1baa74dd17e56
Karol Węgrzyn <karolwg19@gmail.com>
2021-08-06 12:36:24 +0200

    added styles and pie chart

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ function showDateChoose()
 	document.getElementById("datechoose").style.display = 'block';
 	
 }
+
+function sumOfIncomes()
+{
+	var wynagrodzenie;
+	var odsetki;
+	var allegro;
+	var inne;
+	var suma;
+	wynagrodzenie = document.getElementById("wynagrodzenie").value;
+	odsetki = document.getElementById("odsetki").value;
+	 allegro = document.getElementById("allegro").value;
+	 inne = document.getElementById("inne").value;
+	suma = wynagrodzenie + odsetki + allegro + inne;
+
+	document.getElementById("suma").innerHTML = suma;	
+}
+
+	
+	
