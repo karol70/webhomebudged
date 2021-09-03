65d483efb85acd85b9f79a1c0c6c7f30f66bd7d7
Karol Węgrzyn <karolwg19@gmail.com>
2021-09-03 11:31:34 +0200

    add login and registration in PHP

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+
+session_start();
+
+	if(isset($_SESSION['zalogowany']) && ($_SESSION['zalogowany']==true))
+	{
+		header('Location: menu.php');
+		exit();
+	}
+
+?>
+
 <!DOCTYPE HTML>
 <html lang="pl">
 <head>
@@ -20,41 +32,42 @@
 		
 </head>
 <body>
-
+	
 	<header class="text-light">
 		 <h1 class="text-center display-1 text-uppercase text-weight-bold" >Budżet domowy </h1>
-		<p class="h2 text-center mb-5"> Przejmij kontrolę nad domowymi finansami i ciesz się zaoszczędzonymi pieniędzmi</p>
+		<p class="h2 text-center"> Przejmij kontrolę nad domowymi finansami i ciesz się zaoszczędzonymi pieniędzmi</p>
 	</header>
 	
-	<main>
-	<div class="container align-middle text-light">		
-			<form class="bg-success p-4 w-50 mx-auto " style="--bs-bg-opacity: .5;">
-				<h2 class="h3 text-center mb-3"> Rejestracja </h2>
-				<div class="col-sm-8 col-lg-6 mb-3  mx-auto">
-					<label for="Imie" class="form-label"> Imię: </label>
-					<input type ="text" class="form-control" id="Imie" >		
-				</div>
-				
-				<div class="col-sm-8 col-lg-6 mb-3 w-md-50 mx-auto" >
-					<label for="Email" class="form-label"> Email: </label>
-					<input type ="email" class="form-control" id="Email">
+	<article>
+	<div class="container mt-5 text-light">
+			<form class="bg-success p-4 w-50 mx-auto " style="--bs-bg-opacity: .5;" action="login.php" method="post">
+				<h2 class="h3 text-center mb-3"> Logowanie </h2>
+				<?php
+					if(isset ($_SESSION['udanarejestracja'])) echo '<div class="text-center">'.$_SESSION['udanarejestracja'].'</div>';
+					unset($_SESSION['udanarejestracja']);
+				?>
+				<div class="col-sm-8 col-lg-6 mb-3 mx-auto">
+					<label for="Email" class="form-label"> Adres Email: </label>
+					<input type ="email" class="form-control" id="Email" name="email"/>		
 				</div>
 				
 				<div class="col-sm-8 col-lg-6 mb-3 w-md-50 mx-auto" >
 					<label for="Password" class="form-label"> Hasło: </label>
-					<input type ="password" class="form-control" id="Password">
+					<input type ="password" class="form-control" id="Password" name="password"/>
+				<?php
+					if(isset ($_SESSION['blad'])) echo $_SESSION['blad'];
+				?>
 				</div>
+				
 				<div class="text-center">
-				<button type="submit" class="btn btn-success mx-auto ">Utwórz konto</button>
+				
+					<input type="submit" class="btn btn-success"  value="Zaloguj"/>
+					<div class ="mt-3" id="napis"> Nie masz konta? </div>
+					<a href="register.php"><div class="btn btn-secondary"> Zarejestruj się</div></a>
 				</div>
 			</form>
-			
-				
-	</div>
-		
-			
-		
-	</main>
+		</div>
+	</article>
 	
 	<footer>
 		
@@ -63,7 +76,7 @@
 		</div>
 		
 	</footer>
-
+	
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
 	<script  src ="bootstrap/bootstrap.min.js"></script>
 </body>
